d5e207eb30fdbe0f907df76039fbe83de58d419e
PRESISPREMACHANDRAN <presipremachandran@gmail.com>
2024-01-09 10:22:37 +0530

    changed ui

--- src/Components/AddStudent.jsx
+++ src/Components/AddStudent.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const AddStudent = () => {
   return (
-    <div>AddStudent</div>
+    <div>
+        <label htmlFor="" className="form-label">Name</label>
+        <input type="text" className="form-control" />
+        <button className="btn btn-success">Add</button>
+    </div>
   )
 }
 
