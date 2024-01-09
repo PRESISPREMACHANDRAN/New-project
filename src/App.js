73bd9e389aea7f2ffaa2d0e2eab6b9832469d52a
PRESISPREMACHANDRAN <presipremachandran@gmail.com>
2024-01-09 10:33:54 +0530

    changes in ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddStudent from './Components/AddStudent';
+import SearchStudent from './Components/SearchStudent';
 
 function App() {
   return (
     <div className="App">
  <AddStudent/>
+ <SearchStudent/>
     </div>
   );
 }
